3ceee9a9e1bdadbc975a319eec4d45e5a0b3923a
Zachary Zimmerman <zach@zachzimm.com>
2013-02-07 15:32:08 -0500

    Adding one last missing file for old resume.

---
+++ website/contactCaptcha.php
@@ -0,0 +1,34 @@
+<?php
+	include_once('do/common.php');
+	
+	if(isset($_POST["captchaAnswer"]))
+	{
+		$answer = $_POST["captchaAnswer"];
+		
+		/* Currently question is "what is my last name" */
+		
+		if( strtolower($answer) == 'zimmerman' )
+		{
+			printContactInfo();
+		}
+		else
+		{
+				echo 'Wrong';
+		}
+	}
+	
+	function printContactInfo()
+	{
+		$isInUk = checkCountry();
+		/* Put contact information here */
+		echo 'Zachary Zimmerman<br />';
+		if($isInUk == null || !$isInUk)
+			echo 'zach@zachzimm.com<br />';
+		else
+			echo 'zach@zachzimm.co.uk<br />';
+		if($isInUk == null || !$isInUk)
+			echo '+1 812-575-0715';
+		else
+			echo '+44 020 8123 4761';
+	}
+?>
